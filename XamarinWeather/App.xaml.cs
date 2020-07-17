d10d045ccc92b06cb08720ab4581c10160e308a5
Emil Borysiuk <borys1211@gmail.com>
2020-07-17 23:43:26 +0200

    Add project files.

---
+++ XamarinWeather/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinWeather.Services;
+using XamarinWeather.Views;
+
+namespace XamarinWeather
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
